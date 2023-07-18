f470e587cd8ab29ed9071abfdb8283f905020070
dalan <863286+dalanmiller@users.noreply.github.com>
2023-07-18 18:01:32 +1000

    Update postprocess.ts

--- postprocess.ts
+++ postprocess.ts
@@ -1,310 +1,103 @@
-// Example response
+// 2023-07-18 update
+// 2.5 to 4 yo class
+// https://yarraleisure.perfectgym.com.au/clientportal2/Groups/GroupDetailsModal/Details?groupId=6462
+
 // {
-//     "Data": [
-//       {
-//         "Id": 3413,
-//         "Name": "Term 3 - Friday 9.30am Babies",
-//         "MaxUserNumber": 8,
-//         "Level": "Babies",
-//         "Trainer": "Robyn Gosbell",
-//         "Status": "FullBooked",
-//         "BookingIndicator": {
-//           "Indicator": 0,
-//           "Limit": 8,
-//           "Available": 0
-//         },
-//         "ClassDates": [
-//           {
-//             "Dates": null,
-//             "Day": "Mon"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Tue"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Wed"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Thu"
-//           },
-//           {
-//             "Dates": [
-//               "2021-07-16T09:30:00"
-//             ],
-//             "Day": "Fri"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Sat"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Sun"
-//           }
-//         ],
-//         "Durations": [
-//           "PT30M"
-//         ],
-//         "EnrolledUsers": null
+//   "Id": 6462,
+//   "Name": "Tuesday 10.00am Preschoolers",
+//   "ClubId": 3,
+//   "PhotoUrl": null,
+//   "Vacancies": 1,
+//   "BookingIndicator": {
+//     "Indicator": 1,
+//     "Limit": 8,
+//     "Available": 1
+//   },
+//   "Durations": [
+//     "PT30M"
+//   ],
+//   "Level": "Preschoolers",
+//   "Trainer": "Robyn Gosbell",
+//   "TrainerDetails": null,
+//   "Status": "Bookable",
+//   "AboutGroup": null,
+//   "ClassDates": [
+//     {
+//       "Dates": [
+//         "2023-07-18T10:00:00"
+//       ],
+//       "Day": "Tue"
+//     }
+//   ],
+//   "Users": [
+//     {
+//       "GroupInfo": {
+//         "GroupIsFull": true,
+//         "NotMatchingAge": false,
+//         "NotMatchingLevel": false,
+//         "LevelCanBeAssessed": false,
+//         "EnrollmentNotActive": false,
+//         "UserAlreadyInGroup": false,
+//         "GroupEnded": false,
+//         "GroupNotAvailableForApplication": false,
+//         "GroupIsLocked": false,
+//         "InvalidDates": false,
+//         "CanEnrollToGroup": false,
+//         "CanEnrollToGroupWithForce": true
 //       },
-//       {
-//         "Id": 3488,
-//         "Name": "Term 3 - Saturday 10:30am Babies",
-//         "MaxUserNumber": 8,
-//         "Level": "Babies",
-//         "Trainer": "Rory Lewis",
-//         "Status": "FullBooked",
-//         "BookingIndicator": {
-//           "Indicator": 0,
-//           "Limit": 8,
-//           "Available": 0
-//         },
-//         "ClassDates": [
-//           {
-//             "Dates": null,
-//             "Day": "Mon"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Tue"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Wed"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Thu"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Fri"
-//           },
-//           {
-//             "Dates": [
-//               "2021-07-17T10:30:00"
-//             ],
-//             "Day": "Sat"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Sun"
-//           }
-//         ],
-//         "Durations": [
-//           "PT30M"
-//         ],
-//         "EnrolledUsers": null
+//       "IsTransferAvailable": false,
+//       "ActivityCategoryId": 88,
+//       "Status": "Unavailable",
+//       "Reason": null,
+//       "User": {
+//         "Id": 189039,
+//         "FirstName": "Callum",
+//         "LastName": "Miller",
+//         "PhotoUrl": null,
+//         "Initials": "CM",
+//         "IsCurrentUser": false
 //       },
-//       {
-//         "Id": 3521,
-//         "Name": "Term 3 - Sunday 10:00am Babies",
-//         "MaxUserNumber": 8,
-//         "Level": "Babies",
-//         "Trainer": "Mel Tuangthong",
-//         "Status": "FullBooked",
-//         "BookingIndicator": {
-//           "Indicator": 0,
-//           "Limit": 8,
-//           "Available": 0
-//         },
-//         "ClassDates": [
-//           {
-//             "Dates": null,
-//             "Day": "Mon"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Tue"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Wed"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Thu"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Fri"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Sat"
-//           },
-//           {
-//             "Dates": [
-//               "2021-07-18T10:00:00"
-//             ],
-//             "Day": "Sun"
-//           }
-//         ],
-//         "Durations": [
-//           "PT30M"
-//         ],
-//         "EnrolledUsers": null
+//       "BookedSeatNumber": null,
+//       "StandByQueueNumber": null
+//     },
+//     {
+//       "GroupInfo": {
+//         "GroupIsFull": true,
+//         "NotMatchingAge": true,
+//         "NotMatchingLevel": false,
+//         "LevelCanBeAssessed": false,
+//         "EnrollmentNotActive": false,
+//         "UserAlreadyInGroup": false,
+//         "GroupEnded": false,
+//         "GroupNotAvailableForApplication": false,
+//         "GroupIsLocked": false,
+//         "InvalidDates": false,
+//         "CanEnrollToGroup": false,
+//         "CanEnrollToGroupWithForce": true
 //       },
-//       {
-//         "Id": 3334,
-//         "Name": "Term 3 - Tuesday 10:30am Babies",
-//         "MaxUserNumber": 8,
-//         "Level": "Babies",
-//         "Trainer": "Danielle Otter",
-//         "Status": "FullBooked",
-//         "BookingIndicator": {
-//           "Indicator": 0,
-//           "Limit": 8,
-//           "Available": 0
-//         },
-//         "ClassDates": [
-//           {
-//             "Dates": null,
-//             "Day": "Mon"
-//           },
-//           {
-//             "Dates": [
-//               "2021-07-13T10:30:00"
-//             ],
-//             "Day": "Tue"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Wed"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Thu"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Fri"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Sat"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Sun"
-//           }
-//         ],
-//         "Durations": [
-//           "PT30M"
-//         ],
-//         "EnrolledUsers": null
+//       "IsTransferAvailable": false,
+//       "ActivityCategoryId": 88,
+//       "Status": "Unavailable",
+//       "Reason": null,
+//       "User": {
+//         "Id": 189038,
+//         "FirstName": "Daniel",
+//         "LastName": "Miller",
+//         "PhotoUrl": null,
+//         "Initials": "YOU",
+//         "IsCurrentUser": true
 //       },
-//       {
-//         "Id": 3506,
-//         "Name": "Term 3 - Tuesday 11:00am Babies",
-//         "MaxUserNumber": 8,
-//         "Level": "Babies",
-//         "Trainer": "Robyn Gosbell",
-//         "Status": "Bookable",
-//         "BookingIndicator": {
-//           "Indicator": 1,
-//           "Limit": 8,
-//           "Available": 1
-//         },
-//         "ClassDates": [
-//           {
-//             "Dates": null,
-//             "Day": "Mon"
-//           },
-//           {
-//             "Dates": [
-//               "2021-07-13T11:00:00"
-//             ],
-//             "Day": "Tue"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Wed"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Thu"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Fri"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Sat"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Sun"
-//           }
-//         ],
-//         "Durations": [
-//           "PT30M"
-//         ],
-//         "EnrolledUsers": null
-//       },
-//       {
-//         "Id": 3359,
-//         "Name": "Term 3 - Wednesday 11.00am Babies",
-//         "MaxUserNumber": 8,
-//         "Level": "Babies",
-//         "Trainer": "Robyn Gosbell",
-//         "Status": "FullBooked",
-//         "BookingIndicator": {
-//           "Indicator": 0,
-//           "Limit": 8,
-//           "Available": 0
-//         },
-//         "ClassDates": [
-//           {
-//             "Dates": null,
-//             "Day": "Mon"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Tue"
-//           },
-//           {
-//             "Dates": [
-//               "2021-07-14T11:00:00"
-//             ],
-//             "Day": "Wed"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Thu"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Fri"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Sat"
-//           },
-//           {
-//             "Dates": null,
-//             "Day": "Sun"
-//           }
-//         ],
-//         "Durations": [
-//           "PT15M",
-//           "PT30M"
-//         ],
-//         "EnrolledUsers": null
-//       }
-//     ]
-//   }
+//       "BookedSeatNumber": null,
+//       "StandByQueueNumber": null
+//     }
+//   ]
+// }
 
 import {
   readJSON,
   writeJSON,
-  writeCSV,
-} from "https://deno.land/x/flat@0.0.11/mod.ts";
-// import {format} from 'https://cdn.skypack.dev/prettier';
+  // writeCSV,
+} from "https://deno.land/x/flat@0.0.15/mod.ts";
 import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
 
 const octokit = new Octokit({
@@ -312,17 +105,8 @@ const octokit = new Octokit({
 });
 
 const json = await readJSON(Deno.args[0]);
-// writeJSON(
-//     Deno.args[0],
-//     format(
-//         JSON.stringify(json),
-//         {
-//             parser: "json",
-//         }
-//     )
-// )
+// const csvFilePath = "classes.csv";
 
-const csvFilePath = "classes.csv";
 
 // {
 //         "Id": 3359,
@@ -337,21 +121,9 @@ const csvFilePath = "classes.csv";
 //           "Available": 0
 //         },
 
-const currentClasses = [];
-for (const c of json.Data) {
-  const swimClass = {
-    name: c.Name,
-    status: c.Status,
-    available: c.BookingIndicator.Available,
-    limit: c.BookingIndicator.Limit,
-  };
-  currentClasses.push(swimClass);
+if ( json.BookingIndicator.Available >= 1 ) {
 
-  if (
-    swimClass.available > 0 &&
-    (swimClass.name.includes("Saturday") || swimClass.name.includes("Sunday"))
-  ) {
-    try {
+  try {
       await octokit.request("POST /repos/dalanmiller/baby-swim-flat/issues", {
         owner: "dalanmiller",
         repo: "baby-swim-flat",
@@ -371,13 +143,52 @@ for (const c of json.Data) {
     } catch (e) {
       console.log(e);
     }
-  }
 }
 
-if (currentClasses.length) {
-  await writeCSV(csvFilePath, currentClasses);
-} else {
-  await writeCSV(csvFilePath, [
-    { name: null, status: null, available: null, limit: null },
-  ]);
-}
+const jsonFilePath = "last_read.json"
+await writeJSON(jsonFilePath, json);
+
+// const currentClasses = [];
+// for (const c of json.Data) {
+//   const swimClass = {
+//     name: c.Name,
+//     status: c.Status,
+//     available: c.BookingIndicator.Available,
+//     limit: c.BookingIndicator.Limit,
+//   };
+//   currentClasses.push(swimClass);
+
+//   if (
+//     swimClass.available > 0 &&
+//     swimClass.name.includes("Tuesday")
+//   ) {
+//     try {
+//       await octokit.request("POST /repos/dalanmiller/baby-swim-flat/issues", {
+//         owner: "dalanmiller",
+//         repo: "baby-swim-flat",
+//         title: `Class available ${swimClass.name}`,
+//         assignees: ["dalanmiller"],
+//         body: `
+//     # ${swimClass.name}
+//     status: ${swimClass.status}
+//     available: ${swimClass.available}
+//     limit: ${swimClass.limit}
+
+//     https://yarraleisure.perfectgym.com.au/clientportal2/?saquwjj4kvg5rhji23pg24fh4e=mxa3seq5jbe75bguags2gsr3ye#/Groups/3?ageLimitId=3&vacancies=1
+
+//     @dalanmiller
+//     `,
+//       });
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   }
+// }
+
+// if (currentClasses.length) {
+//   await writeCSV(csvFilePath, currentClasses);
+// } else {
+//   await writeCSV(csvFilePath, [
+//     { name: null, status: null, available: null, limit: null },
+//   ]);
+// }
